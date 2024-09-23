2614331af5ed668b17de524ea4230a17d4234eb1
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2024-09-22 21:31:54 -0700

    Tut-05
    image appear/disappear

--- tutorials/05-com_javascript_interaction/js/main.js
+++ tutorials/05-com_javascript_interaction/js/main.js
@@ -16,31 +16,33 @@ header.onclick = function(){
 }
 
 document.querySelector("#image-0").addEventListener("click", function(){
-    document.querySelector("#image-0").style.display = "none";
+    document.querySelector("#image-01").style.visibility = "visible";
+    alert("EXPLODE");
 })
 
 document.querySelector("#image-01").addEventListener("click", function(){
-    document.querySelector("#image-01").style.display = "none";
+    document.querySelector("#image-02").style.visibility = "visible";
 })
 
 document.querySelector("#image-02").addEventListener("click", function(){
-    document.querySelector("#image-02").style.display = "none";
+    document.querySelector("#image-03").style.visibility = "visible";
 })
 
 document.querySelector("#image-03").addEventListener("click", function(){
-    document.querySelector("#image-03").style.display = "none";
+    document.querySelector("#image-04").style.visibility = "visible";
+    s
 })
 
 document.querySelector("#image-04").addEventListener("click", function(){
-    document.querySelector("#image-04").style.display = "none";
+    document.querySelector("#image-05").style.visibility = "visible";
 })
 
 document.querySelector("#image-05").addEventListener("click", function(){
-    document.querySelector("#image-05").style.display = "none";
+    document.querySelector("#image-06").style.visibility = "visible";
 })
 
 document.querySelector("#image-06").addEventListener("click", function(){
-    document.querySelector("#image-06").style.display = "none";
+    document.querySelector("#image-07").style.visibility = "visible";
 })
 
 document.querySelector("#image-07").addEventListener("click", function(){
