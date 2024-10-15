58b4ad10c691df3bd0a1113edf02688b50d3b6ed
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2024-10-14 20:06:56 -0700

    omg this shit is FINALLY working :/
    -i hate this

--- tutorials/07-three,js/js/main.js
+++ tutorials/07-three,js/js/main.js
@@ -1,67 +1,54 @@
-// Basic Three.JS scene from documentation, importing Three.JS through a CDN 
-// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
-//~~~~~~~GLOBAL DECLARATIONS~~~~~~
+//===================== imported Addons
+import * as THREE from 'three';
+import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
+
+//....... GLOBAL VARIABLES
+let scene, camera, renderer, cube;
 
 function init(){
-    //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
-        import * as THREE from 'three';
-
-    // Import add-ons
-        import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
-        import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
-
-
-
-    // ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    //===================== SCENE
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    document.body.appendChild( renderer.domElement );
 
     // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
-    const controls = new OrbitControls(camera, renderer.domElement);
-    //const loader = new GLTFLoader(); // to load 3d models
+    const controls = new OrbitControls(camera, renderer.domElement); 
+    const loader = new GLTFLoader(); // to load 3d models
 
-    // ~~~~~~~~~~~~~~~~ 3-D stuff ~~~~~~~~~~~~~~~~
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    //===================== Geometry
+    const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
     const texture = new THREE.TextureLoader().load('texture/ice.jpg');
     const material = new THREE.MeshBasicMaterial({ map: texture });
-    const cube = new THREE.Mesh(geometry, material);
-    const cube = new THREE.Mesh(geometry, material);
+    cube = new THREE.Mesh( geometry, material );
     scene.add(cube);
 
-
-    // ~~~~~~~~~~~~~~~~ camera ~~~~~~~~~~~~~~~~
-    camera.position.x = 5;
+    //===================== CAMERA
+    camera.position.z = 5;
 
 }
 
-// ~~~~~~~~~~~~~~~~ Animation ~~~~~~~~~~~~~~~~
-function animate(){
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+//===================== ANIAMTION
+function animate() {
+    
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
 
-    renderer.render(scene, camera);
+    renderer.render( scene, camera );
 }
 
-// ~~~~~~~~~~~~~~~~ WINDOW RESIZE ~~~~~~~~~~~~~~~~
-function onWindowResize(){
+function onWindowResize() {
     camera.aspect = window.innerWidth / windown.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
-
-
-// ------------------- RUN FUNCTIONS-------------------
-
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener('resize', onWindowResize, false);
 
 init();
-
-animate(); // execute animation function
+animate();
