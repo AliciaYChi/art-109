adc3dcb57e9367875e7818505e856f80a51708d3
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2024-11-04 21:32:53 -0800

    TUT-9
    -animatios to play GRAHH

--- tutorials/02-css_essentials/js/main.js
+++ tutorials/02-css_essentials/js/main.js
@@ -6,7 +6,7 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders
 
 
 //....... GLOBAL VARIABLES
-let scene, camera, torus, capsule, prism_poupy, prism_poupy1, prism_poupy2, prism_poupy3, prism_poupy4, mixer, dog, dog2, envi, mother;
+let scene, camera, torus, capsule, prism_poupy, prism_poupy1, prism_poupy2, prism_poupy3, prism_poupy4, mixer, envi, mother, mother2;
 let actionPant, actionTail;
 let sceneContainer = document.querySelector("#scene-container");
 
@@ -45,54 +45,16 @@ function init(){
     // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
     const controls = new OrbitControls(camera, renderer.domElement); 
     const loader = new GLTFLoader(); // to load 3d models
-    loader.load('assets/dog_shiny.gltf', function (gltf) {
-        dog = gltf.scene;
-        scene.add(dog);
-        dog.scale.set(1, 1, 1);
-        dog.position.y = .5; 
-        dog.position.x = -4.5; 
 
-        // animation!
-        mixer = new THREE.AnimationMixer(dog); // initiate mixer
-        const clips = gltf.animations;  // load all clips
-
-        // load + play pant animation
-        const clipPant = THREE.AnimationClip.findByName(clips, 'pant');
-        const actionPant = mixer.clipAction(clipPant);
-        actionPant.play();
-
-        // load + play tail animation
-        const clipTail = THREE.AnimationClip.findByName(clips, 'tail');
-        const actionTail = mixer.clipAction(clipTail);
-        actionTail.play();
-    });
-    loader.load('assets/dog_shiny.gltf', function (gltf) {
-        dog2 = gltf.scene;
-        scene.add(dog2);
-        dog2.scale.set(2, 2, 2);
-        dog2.position.y = -0.5; 
-        dog2.position.x = 5; 
-
-        // animation!
-        mixer = new THREE.AnimationMixer(dog2); // initiate mixer
-        const clips = gltf.animations;  // load all clips
-
-        // load + play pant animation
-        const clipPant = THREE.AnimationClip.findByName(clips, 'pant');
-        const actionPant = mixer.clipAction(clipPant);
-        actionPant.play();
-
-        // load + play tail animation
-        const clipTail = THREE.AnimationClip.findByName(clips, 'tail');
-        const actionTail = mixer.clipAction(clipTail);
-        actionTail.play();
-    });
-
-    loader.load('assets/mother.gtfl', function (gltf) {
+    loader.load('assets/mother.gltf', function (gltf) {
         mother = gltf.scene;
         scene.add(mother);
-        mother.scale.set(.01, .01, .01);
- 
+        mother.scale.set(.07, .07, .07);
+        mother.position.z = -20; 
+        mother.position.x = 20; 
+        mother.position.y = 5;
+        mother.rotation.y = 360;
+
         // animation!
         mixer = new THREE.AnimationMixer(mother); // initiate mixer
         const clips = gltf.animations;  // load all clips
@@ -114,8 +76,67 @@ function init(){
 
         // load + play hand1 animation
         const clipHand1 = THREE.AnimationClip.findByName(clips, 'hand1');
-        const actionHand1 = mixer.clipAction(clipHand);
-        actionHand.play();   
+        const actionHand1 = mixer.clipAction(clipHand1);
+        actionHand1.play();   
+
+        // load + play hand2 animation
+        const clipHand2 = THREE.AnimationClip.findByName(clips, 'hand2');
+        const actionHand2 = mixer.clipAction(clipHand2);
+        actionHand2.play();
+     
+        // load + play hand3 animation
+        const clipHand3 = THREE.AnimationClip.findByName(clips, 'hand3');
+        const actionHand3 = mixer.clipAction(clipHand3);
+        actionHand3.play();
+
+        // load + play hand4 animation
+        const clipHand4 = THREE.AnimationClip.findByName(clips, 'hand4');
+        const actionHand4 = mixer.clipAction(clipHand4);
+        actionHand4.play();
+
+        // load + play hand5 animation
+        const clipHand5 = THREE.AnimationClip.findByName(clips, 'hand5');
+        const actionHand5 = mixer.clipAction(clipHand5);
+        actionHand5.play();
+
+        // load + play hand6 animation
+        const clipHand6 = THREE.AnimationClip.findByName(clips, 'hand6');
+        const actionHand6 = mixer.clipAction(clipHand6);
+        actionHand6.play();
+    });
+
+    loader.load('assets/mother.gltf', function (gltf) {
+        mother2 = gltf.scene;
+        scene.add(mother2);
+        mother2.scale.set(.07, .07, .07);
+        mother2.position.z = -20; 
+        mother2.position.x = -20; 
+        mother2.position.y = 5;
+        mother2.rotation.y = 180;
+
+        // animation!
+        mixer = new THREE.AnimationMixer(mother2); // initiate mixer
+        const clips = gltf.animations;  // load all clips
+
+        // load + play leg1 animation
+        const clipLeg1 = THREE.AnimationClip.findByName(clips, 'leg1');
+        const actionLeg1 = mixer.clipAction(clipLeg1);
+        actionLeg1.play();
+
+        // load + play leg2 animation
+        const clipLeg2 = THREE.AnimationClip.findByName(clips, 'leg2');
+        const actionLeg2 = mixer.clipAction(clipLeg2);
+        actionLeg2.play();
+
+        // load + play hand animation
+        const clipHand = THREE.AnimationClip.findByName(clips, 'hand');
+        const actionHand = mixer.clipAction(clipHand);
+        actionHand.play();
+
+        // load + play hand1 animation
+        const clipHand1 = THREE.AnimationClip.findByName(clips, 'hand1');
+        const actionHand1 = mixer.clipAction(clipHand1);
+        actionHand1.play();   
 
         // load + play hand2 animation
         const clipHand2 = THREE.AnimationClip.findByName(clips, 'hand2');
@@ -151,14 +172,7 @@ function init(){
         envi.translateY(-4);
         envi.translateZ(-8);  
     });
-    loader.load('assets/mother', function (gltf){
-        mother= gltf.scene;
-        scene.add(mother);
-        mother.scale.set(.1,.1,.1);
-        mother.translateX(0);
-        mother.translateY(-0.2);
-    mother.translateZ(-18);  
-    });
+
 
     loader.load('assets/prisim_poupy.gltf', function (gltf){
         prism_poupy = gltf.scene;
@@ -205,7 +219,6 @@ function init(){
 
 let mouseDown = false;
 
-
 document.querySelector('#scene-container').addEventListener('mousedown', () => {
     console.log("mouse clicked");
     mouseDown = true;
@@ -241,17 +254,15 @@ const clock = new THREE.Clock();
 
  function animate() {
     //  requestAnimationFrame(animate);
-    if (dog) { // check to see if model loaded first
+    if (mother) { // check to see if model loaded first
         mixer.update(clock.getDelta());
-        dog.rotation.y = Math.sin(Date.now() / 2000) * 1.2;
-
     }
 
-    if (dog2) { // check to see if model loaded first
+    if (mother2) { // check to see if model loaded first
         mixer.update(clock.getDelta());
-        dog2.rotation.x = Math.sin(Date.now() / 4000) * 1.2;
-
     }
+    
+
 
     if (prism_poupy){
         prism_poupy.rotation.x -= 0.007;
